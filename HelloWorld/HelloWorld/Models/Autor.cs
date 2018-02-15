7804dc8fa380800c1d429476443d5c6beae1cb37
Wellington Antonio Zaneze <wellingtonantonio_zaneze@live.com>
2018-02-14 22:03:36 -0200

    Models added

---
+++ HelloWorld/HelloWorld/Models/Autor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace HelloWorld.Models
+{
+    public class Autor : INotifyPropertyChanged
+    {
+
+        int id;
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(id)));
+            }
+        }
+
+        string nome;
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+            set
+            {
+                nome = value;
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(nome)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ HelloWorld/HelloWorld/Models/Book.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace HelloWorld.Models
+{
+    public class Book : INotifyPropertyChanged
+    {
+
+        int id;
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(id)));
+            }
+        }
+
+        string titulo;
+        public string Titulo
+        {
+            get
+            {
+                return titulo;
+            }
+            set
+            {
+                titulo = value;
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(titulo)));
+            }
+        }
+
+        Autor autor;
+        public Autor Autor
+        {
+            get
+            {
+                return autor;
+            }
+            set
+            {
+                autor = value;
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(autor)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+
+    }
+}
